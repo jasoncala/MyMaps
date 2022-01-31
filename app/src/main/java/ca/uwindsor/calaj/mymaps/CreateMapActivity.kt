567ad44f30b46f0d9425339d1e725789bc70ab57
Jason Cala <calaj@uwindsor.ca>
2022-01-30 23:23:35 -0500

    implemented create place functionality in add map

--- app/src/main/java/ca/uwindsor/calaj/mymaps/CreateMapActivity.kt
+++ app/src/main/java/ca/uwindsor/calaj/mymaps/CreateMapActivity.kt
@@ -1,7 +1,13 @@
 package ca.uwindsor.calaj.mymaps
 
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -10,11 +16,14 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import ca.uwindsor.calaj.mymaps.databinding.ActivityCreateMapBinding
+import com.google.android.gms.maps.model.Marker
 
+private const val TAG = "CreateMapActivity"
 class CreateMapActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityCreateMapBinding
+    private var markers: MutableList<Marker> = mutableListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,9 +51,41 @@ class CreateMapActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
+        mMap.setOnInfoWindowClickListener { markerToDelete ->
+            Log.i(TAG, "OnInfoWindowClickListener - delete marker")
+            markers.remove(markerToDelete)
+            markerToDelete.remove()
+        }
+
+        mMap.setOnMapLongClickListener { latLng ->
+            Log.i(TAG, "setOnMapLongClickListener")
+            showAlertDialog(latLng)
+        }
         // Add a marker in Sydney and move the camera
         val sydney = LatLng(-34.0, 151.0)
         mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+
+    private fun showAlertDialog(latLng: LatLng) {
+        val placeFormView = LayoutInflater.from(this).inflate(R.layout.dialog_create_place, null)
+        val dialog =
+            AlertDialog.Builder(this)
+                .setTitle("Create a marker")
+                .setView(placeFormView)
+                .setNegativeButton("Cancel", null)
+                .setPositiveButton("Ok",null)
+                .show()
+        dialog.getButton(DialogInterface.BUTTON_POSITIVE).setOnClickListener{
+            val title = placeFormView.findViewById<EditText>(R.id.etTitle).text.toString()
+            val description = placeFormView.findViewById<EditText>(R.id.etDescription).text.toString()
+            if (title.trim().isEmpty() || description.trim().isEmpty()){
+                Toast.makeText(this, "Place must have non-empty title and description", Toast.LENGTH_LONG).show()
+                return@setOnClickListener
+            }
+            val marker = mMap.addMarker(MarkerOptions().position(latLng).title(title).snippet(description))
+            markers.add(marker)
+            dialog.dismiss()
+        }
     }
 }
